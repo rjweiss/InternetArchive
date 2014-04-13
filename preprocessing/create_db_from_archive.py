a48b9382ed1608d611c03533646b75921e7edc0c
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2014-04-13 10:05:50 -0700

    Enabled multiprocessing for a performance increase.

--- preprocessing/create_db_from_archive.py
+++ preprocessing/create_db_from_archive.py
@@ -1,7 +1,15 @@
-import os, sys, re, csv, argparse
-from datetime import datetime
-from pymongo import MongoClient
-from pymongo import errors
+#!/usr/bin/python
+'''
+Description: This script processes a directory of Internet Archive-formatted closed captioning files, extracts metadata, constructs a flattened text string of a entire program, and inserts into a MongoDB collection.
+Author: Rebecca Weiss
+'''
+import os, sys, re, csv, argparse, multiprocessing
+from datetime           import datetime
+from pymongo            import MongoClient
+from pymongo            import errors
+from stat               import S_ISREG
+from multiprocessing    import Pool, freeze_support, Process, JoinableQueue
+from hashlib            import md5
 
 def MongoConnect(db_name, collection_name):
     try: 
@@ -15,19 +23,20 @@ def MongoConnect(db_name, collection_name):
 class InternetArchiver(object):
 
 	#def __init__(self):
-	# connect to mongo here	
+	# TODO: connect to mongo here?
 
 	def extract(self, infile, mongo_collection):	
+		processed = False		
 
-		processed = False
 		try:
-			mongo_document = self._process_cc(infile)			
+			mongo_document = self._process_cc(infile)
 			processed = True
 		except IOError as e:
 			sys.stderr.write('IOError: ' + str(e))
 
-		if processed:			
+		if processed:
 			mongo_collection.save(mongo_document)
+            # TODO Fix this to catch errors
 			# try:
 			# 	mongo_collection.save(mongo_document)
 			# except pymongo.errors.OperationFailure:
@@ -46,13 +55,13 @@ class InternetArchiver(object):
 		except IOError as e:
 			sys.stderr.write('IOError: ' + str(e))
 		
-		metadata = self._get_metadata(infile)	
+		metadata = self._get_metadata(infile)
 
 		return {
 			'filename' : infile,
 			'channel' : metadata['channel'],
-			'date' : metadata['date'].isoformat(), # TODO: This is just a string...need to make sure that Mongo recognizes this as a date object
-			'time' : metadata['time'].isoformat(), # TODO: This is just a string...need to make sure that Mongo recognizes this as a time object
+			'date' : metadata['date'].isoformat(),
+			'time' : metadata['time'].isoformat(),
 			'show': metadata['show'],
 			'cc_text' : self._get_cc_data(ccdata)
 		}
@@ -61,17 +70,20 @@ class InternetArchiver(object):
 		'''
 		This helper function takes in a filename string from the IArchive and returns metadata (Channel, Date, Show)
 		'''	
-		file_pattern = re.compile(r"^rawdata\/([A-Z0-9]+)_(\d+)_(\d+)_(.+)\.cc5\.txt$")
+		file_pattern = re.compile(r"rawdata\/([A-Z0-9]+)_(\d+)_(\d+)_(.+)\.cc5\.txt$")
+
 
 		match = re.search(file_pattern, filename_string)
 		date = datetime.strptime(match.group(2), "%Y%m%d") #date
-		time = datetime.strptime(match.group(3), "%H%M%S") #time, NOT ACCURATE
+		time = datetime.strptime(match.group(3), "%H%M%S") #time, NOT ACCURATE?
 
 		return {
-		'channel': match.group(1),
-		'date' : date.date(),
-		'time': time.time(),
-		'show' : str(match.group(4))		
+			'channel': match.group(1),
+			'date' : date.date(),
+			'time': time.time(),
+			'date': date,
+			'time': time, #TODO returns from 1900...need to make it just return time?
+			'show' : str(match.group(4))		
 		}
 
 	def _get_cc_data(self, ccdata_list):
@@ -99,11 +111,62 @@ class InternetArchiver(object):
 		
 		return cc_line.replace('"', '``')
 
+def list_files(data_dir):
+	for f in os.listdir(data_dir):
+		pathname = os.path.join(data_dir, f)
+		yield pathname
+
+def files_to_process(data_dir, cc_type):
+    #filecount = 0
+	for f in list_files(data_dir):
+		if f.endswith(cc_type):
+			try: 
+				fileinfo = os.stat(f)
+			except OSError:
+				pass
+			else:
+				if S_ISREG(fileinfo.st_mode):
+#					filecount +=1
+					yield f
+                        # TODO: Return filecount?
+
+def extract_and_insert(current_file, mongo_collection, archiver):
+	try:
+		document = mongo_collection.find_one({"filename": current_file})
+	except AttributeError as e: # TODO: This is the wrong error.
+		sys.stderr("Error ({error}), exiting.\n".format(error=e))
+		sys.exit()
+
+	if document:
+		print("File has already been processed.")
+		return
+	else:
+		valid = archiver.extract(current_file, mongo_collection)
+        
+		if valid:
+			sys.stdout.write('Processing {filename}\n'.format(filename=current_file))
+		else:
+			sys.stderr.write('Extraction failure, skipping {filename}'.format(filename=current_file))
+			return            
+		sys.stdout.flush()
+		return    
+	return
+    
+def extract_and_insert_process(queue, mongo_collection):
+	arc = InternetArchiver()
+	while True:
+		current_file = queue.get()
+		if current_file == "done":
+			break
+		extract_and_insert(current_file, mongo_collection, arc)
+		queue.task_done()        
+	queue.task_done()
+        
+            
 def main(data_dir, mdb, mc):
 
 	sys.stdout.write('Connecting to Mongo...')
 	sys.stdout.flush()
-
 	connection, db, collection = MongoConnect(mdb, mc)
 
 	if connection:
@@ -112,34 +175,36 @@ def main(data_dir, mdb, mc):
 		sys.stdout.write('failed.\n')
 		sys.exit()
 
-	arc = InternetArchiver()
-
 	if not os.path.exists(data_dir):
 		sys.exit('ERROR: Directory does not exist.')
 	
-	for f in os.listdir(data_dir):
-		print f
-		if f.endswith('.cc5.txt'):
-			current_file = os.path.join(data_dir, f)
-			valid = arc.extract(current_file, collection)
-			if valid:
-				print f
-			else:
-				print foo
-				sys.exit()
-					                        
-	print 'Finished processing request.'
+	num_procs = multiprocessing.cpu_count()
+	queue = JoinableQueue()
+	files = files_to_process(data_dir, '.cc5.txt')
+	#sys.stdout.write("{filecount} files to process".format(filecount=len(files)))
+    
+	for i in range(0, num_procs):
+		sys.stdout.write("Creating processes {num}...\n".format(num=i))
+		p = Process(target=extract_and_insert_process, args=(queue, collection))
+		p.start()
+            
+	for f in files:
+		queue.put(f)        
+        
+	for i in range(0, num_procs):
+		queue.put("done")
+		queue.join()
+    					                        
+	sys.stdout.write("Processing complete.\n")
 
-
-if __name__ == "__main__":
-	
+if __name__ == "__main__":	
 	parser = argparse.ArgumentParser(description='Process Internet Archive files')
 	parser.add_argument('-d','--datadir', help='data directory', default='data/', required=True)
 	parser.add_argument('-mdb','--mongodb', help='mongo database', default='test', required=False)
-	parser.add_argument('-mc','--mongocoll', help='mongo collection', default='test', required=False)
-	
+	parser.add_argument('-mc','--mongocoll', help='mongo collection', default='test', required=False)	
 	args = vars(parser.parse_args())
-
+    
+	freeze_support()
 	if args['mongodb'] and args['mongocoll']:
 		main(args['datadir'], args['mongodb'], args['mongocoll'])
 	else:
