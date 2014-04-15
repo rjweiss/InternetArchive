38604ed0e1adbabb835b78b8203374bfa5c61f72
Rebecca Weiss <rebecca.j.weiss@gmail.com>
2014-04-15 14:45:06 -0400

    Added logging.

--- preprocessing/create_db_from_archive.py
+++ preprocessing/create_db_from_archive.py
@@ -2,7 +2,6 @@
 '''
 Description: This script processes a directory of Internet Archive-formatted closed captioning files, extracts metadata, constructs a flattened text string of a entire program, and inserts into a MongoDB collection.
 Author: Rebecca Weiss
-TODO: Need to add proper logging...is Mongo insertion the bottleneck? Seems to take too long...
 '''
 import os, sys, re, csv, argparse, multiprocessing, hashlib
 from datetime           import datetime
@@ -10,17 +9,20 @@ from pymongo            import MongoClient
 from pymongo            import errors
 from multiprocessing    import Process, JoinableQueue
 from hashlib            import md5
-import pprint
+import logging
 
 def MongoConnect(db_name, collection_name):
-    try: 
-        connection = MongoClient()
-        database = connection[db_name]
-        collection = database[collection_name]
-        index_exists = index_check(collection)
-        return (connection, database, collection)
-    except pymongo.errors.OperationFailure:
-        return False        
+	try: 
+		connection = MongoClient()
+		database = connection[db_name]
+		collection = database[collection_name]
+		index_exists = index_check(collection)
+		if index_exists:
+			return (connection, database, collection)
+		else:
+			return False
+	except pymongo.errors.OperationFailure:
+		return False        
 
 def index_check(collection):
 	collection.ensure_index("md5", unique=True)
@@ -29,7 +31,6 @@ def index_check(collection):
 		return True
 	else:
 		return False
-
 
 def list_files(data_dir):
 	for f in os.listdir(data_dir):
@@ -43,57 +44,55 @@ def files_to_process(data_dir, cc_type):
 			yield f
     # TODO: Return filecount?
 
-def extract_and_insert(current_file, mongo_collection, archiver):
+def extract_and_insert(current_filename, mongo_collection, archiver, process_logger):
 	m = hashlib.md5()
-	m.update(current_file)
+	m.update(current_filename)
 	try:
 		cursor = mongo_collection.find({"md5": m.hexdigest()}).limit(1)
 	except AttributeError as e: # TODO: This is the wrong error.
-		sys.stderr("Error ({error}), exiting.\n".format(error=e))
+		process_logger.error('Error ({error}), exiting.'.format(error=e))
 		sys.exit()
 
 	document_exists = cursor.count()
 
 	if document_exists > 0:
-		sys.stderr.write("{fname} has already been processed.\n".format(fname=current_file))
+		process_logger.debug('{fname} has already been processed'.format(fname=current_filename))
 		return
 	else:
-		extracted_document = archiver.extract(current_file, mongo_collection)
-        
+		extracted_document = archiver.extract(current_filename, mongo_collection)
+		shortened_filename = ''.join(current_filename.split('/')[-1])
+
 		if extracted_document:
-			sys.stdout.write('Processed {filename}\n'.format(filename=current_file))
+			process_logger.debug('Processed {filename}'.format(filename=shortened_filename))
 		else:
-			sys.stderr.write('Extraction failure, skipping {filename}\n'.format(filename=current_file))
-            
-		sys.stdout.flush()
+			process_logger.error('Extraction failure, skipping {filename}'.format(filename=shortened_filename))
 		return    
-	return
     
-def extract_and_insert_process(queue, mongo_collection):
-	arc = InternetArchiver()
+def extract_and_insert_process(queue, mongo_collection, process_logger, extractor_logger):
+	arc = InternetArchiver(extractor_logger=extractor_logger)
 	while True:
 		current_file = queue.get()
 		if current_file is None:
 			break
-		extract_and_insert(current_file, mongo_collection, arc)
+		extract_and_insert(current_file, mongo_collection, arc, process_logger)
 		queue.task_done()        
 	queue.task_done()
     
 class InternetArchiver(object):
 
-	#def __init__(self):
-	# TODO: connect to mongo here?
+	def __init__(self, extractor_logger):
+		self.logger = extractor_logger
 
     # Main extraction function
-	def extract(self, infile, mongo_collection):	
-		processed = False		
+	def extract(self, infile, mongo_collection):
+		processed = False
 		m = hashlib.md5()
 
 		try:
 			mongo_document = self._process_cc(infile)
 			processed = True
-		except IOError as e:
-			sys.stderr.write('IOError: ' + str(e))
+		except AttributeError as e:
+			self.logger('Processing error {file}'.format(file=infile))
 
 		if processed:
 			m.update(mongo_document['filename'])
@@ -112,7 +111,7 @@ class InternetArchiver(object):
 			with open(infile, 'rb') as ccfile:
 				ccdata = ccfile.readlines()
 		except IOError as e:
-			sys.stderr.write('IOError: ' + str(e))
+			self.logger.error('IOError: {error}'.format(error=e))
 		
 		metadata = self._get_metadata(infile)
 
@@ -153,23 +152,38 @@ class InternetArchiver(object):
 		try:
 			cc_line = match.group(5).replace('"', '``')
 		except AttributeError as e:
-			sys.stderr.write('regex processing error: +' + inputline + str(e))
+			self.logger.error('regex processing error: {inputline} ({error})'.format(inputline=inputline, error=e))
+#			sys.stderr.write('regex processing error: +' + inputline + str(e))
 		
 		return cc_line
 
-def main(data_dir, mdb, mc):
-	sys.stdout.write('Connecting to Mongo...')
-	sys.stdout.flush()
+def main(data_dir, mdb, mc, logfile):
+	logging.basicConfig(level 		= logging.DEBUG,
+	                	format 		= '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+	                	datefmt 	= '%m-%d %H:%M',
+	                	filename 	= logfile,
+	                	filemode 	= 'w')
+	
+	console = logging.StreamHandler()
+	console.setLevel(logging.INFO)
+	formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+	console.setFormatter(formatter)
+	logging.getLogger('').addHandler(console)
+	archiver_process_logger = logging.getLogger('archiver.process')
+	archiver_extractor_logger = logging.getLogger('archiver.extractor')
+
+	logging.info('Connecting to Mongo database {mdb}, collection {mc}...'.format(mdb=mdb, mc=mc))
 	connection, db, collection = MongoConnect(mdb, mc)
 
 	if connection:
-		sys.stdout.write('success.\n')
+		logging.info('Successfully connected to Mongo.')
 	else:
-		sys.stdout.write('failed.\n')
+		logging.info('Failed to connect to Mongo.')
 		sys.exit()
 
 	if not os.path.exists(data_dir):
-		sys.exit('ERROR: Directory does not exist.\n')
+		logging.error('Data directory does not exist.')
+		sys.exit()
 	
 	num_procs = multiprocessing.cpu_count()
 	queue = JoinableQueue()
@@ -177,8 +191,8 @@ def main(data_dir, mdb, mc):
 	#sys.stdout.write("{filecount} files to process".format(filecount=len(files)))
     
 	for i in range(0, num_procs):
-		sys.stdout.write("Creating processes {num}...\n".format(num=i))
-		p = Process(target=extract_and_insert_process, args=(queue, collection))
+		logging.info('Creating process {num}'.format(num=i + 1))
+		p = Process(target=extract_and_insert_process, args=(queue, collection, archiver_process_logger, archiver_extractor_logger))
 		p.start()
             
 	for f in files:
@@ -187,18 +201,19 @@ def main(data_dir, mdb, mc):
 	for i in range(0, num_procs):
 		queue.put(None)
 		queue.join()
-    					                        
-	sys.stdout.write("Processing complete.\n")
+
+	logging.info('Processing complete.')
 
 if __name__ == "__main__":	
 	parser = argparse.ArgumentParser(description='Process Internet Archive files')
-	parser.add_argument('-d',   '--datadir',    help='data directory',      default='data/',    required=True)
-	parser.add_argument('-mdb', '--mongodb',    help='mongo database',      default='test',     required=False)
-	parser.add_argument('-mc',  '--mongocoll',  help='mongo collection',    default='test',     required=False)	
+	parser.add_argument('-d',   '--datadir',    help='data directory',      default='data/',    	required=True)
+	parser.add_argument('-mdb', '--mongodb',    help='mongo database',      default='test',     	required=False)
+	parser.add_argument('-mc',  '--mongocoll',  help='mongo collection',    default='test',     	required=False)
+	parser.add_argument('-log', '--logfile',	help='logfile path', 		default='archiver.log', required=True)
 	args = vars(parser.parse_args())
     
 	if args['mongodb'] and args['mongocoll']:
-		main(args['datadir'], args['mongodb'], args['mongocoll'])
+		main(data_dir=args['datadir'], mdb=args['mongodb'], mc=args['mongocoll'], logfile=args['logfile'])
 	else:
-		main(args['datadir'], mongodb=None, mongocoll=None)
+		main(data_dir=args['datadir'], mdb=None, mc=None, logfile=args['logfile'])
 
